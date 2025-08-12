77db37bdd84ba91cd0d4cb0529a31c51c245a353
ananiko9 <ananiko@d4e.gg>
2025-08-12 14:57:55 +0400

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { RouterProvider } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { router } from "./router";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
 }
 
 export default App;
